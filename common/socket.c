0e343f3c37b2b2cb8a7c63eab80a98257f526f07
Matt Mitchell <mmitchell@bluecedar.com>
2018-02-22 11:29:34 -0500

    Removed loopback in socket accept too

--- common/socket.c
+++ common/socket.c
@@ -368,7 +368,7 @@ int socket_accept(int fd, uint16_t port)
 
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	addr.sin_port = htons(port);
 
 	addr_len = sizeof(addr);
